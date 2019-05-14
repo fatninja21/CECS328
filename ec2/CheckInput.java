895735458a79a8f386dd4fd823d4da88bd86af23
fatninja21 <43802253+fatninja21@users.noreply.github.com>
2019-05-14 13:29:48 -0700

    uploaded

---
+++ ec2/CheckInput.java
@@ -0,0 +1,54 @@
+package ec2;
+
+import java.util.Scanner;
+
+public class CheckInput {
+	
+	/**
+	 * Checks if the inputted value is an integer and 
+	 * within the specified range (ex: 1-10)
+	 * @param low lower bound of the range.
+	 * @param high upper bound of the range.
+	 * @return the valid input.
+	 */
+	public static int getIntRange( int low, int high ) {
+		Scanner in = new Scanner( System.in );
+		int input = 0;
+		boolean valid = false;
+		while( !valid ) {
+			if( in.hasNextInt() ) {
+				input = in.nextInt();
+				if( input <= high && input >= low ) {
+					valid = true;
+				} else {
+					System.out.println( "Invalid Range." );
+				}
+			} else {
+				in.next(); //clear invalid string
+				System.out.println( "Invalid Input." );
+			}
+		}
+		return input;
+	}
+	
+	public static int getPositiveInt( ) {
+		Scanner in = new Scanner( System.in );
+		int input = 0;
+		boolean valid = false;
+		while( !valid ) {
+			if( in.hasNextInt() ) {
+				input = in.nextInt();
+				if( input >= 0 ) {
+					valid = true;
+				} else {
+					System.out.println( "Invalid Range." );
+				}
+			} else {
+				in.next(); //clear invalid string
+				System.out.println( "Invalid Input." );
+			}
+		}
+		return input;
+	}
+
+}
